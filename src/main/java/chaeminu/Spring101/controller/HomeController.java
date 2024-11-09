5aeff69410a93279587a64b3d2d595072d9ca72f
choiminwoo <dandalf@o.cnu.ac.kr>
2024-11-09 15:39:10 +0900

    회원 관리 예제 - 웹 MVC 개발

---
+++ src/main/java/chaeminu/Spring101/controller/HomeController.java
@@ -0,0 +1,13 @@
+package chaeminu.Spring101.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/chaeminu/Spring101/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package chaeminu.Spring101.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

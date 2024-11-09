455aa9aee2c2c5f6fa5d3fc04727ea38b708aa80
choiminwoo <dandalf@o.cnu.ac.kr>
2024-11-09 12:58:47 +0900

    스프링 웹 개발 기초

--- src/main/java/chaeminu/Spring101/controller/HelloController.java
+++ src/main/java/chaeminu/Spring101/controller/HelloController.java
@@ -3,6 +3,8 @@ package chaeminu.Spring101.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,38 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello World");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name", required = false) String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String name;
+
     }
 }

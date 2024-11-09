0026363eb63fdbe993f4ad845e1c824e3015235f
choiminwoo <dandalf@o.cnu.ac.kr>
2024-11-09 12:28:24 +0900

    프로젝트 환경설정

---
+++ src/main/java/chaeminu/Spring101/controller/HelloController.java
@@ -0,0 +1,14 @@
+package chaeminu.Spring101.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Hello World");
+        return "hello";
+    }
+}
